c85dd6776a35b34f863725bb99597c0f416eec52
abyo <Iplayelemshaman@gmail.com>
2017-03-14 23:43:26 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 500
+        self.screen_height = 1000
+        self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 0.44
